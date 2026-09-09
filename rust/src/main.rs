20abb1d91880d6671812456422f9ba08b9b56366
akarsh <akarsh98@gmail.com>
2026-09-10 00:57:13 +0200

    buffered reading on rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+
+fn main() -> std::io::Result<()> {
+    let path = "../data/weather_stations.csv";
+    let f = File::open(path)?;
+    let mut reader = BufReader::new(f);
+    let mut line = String::new();
+    while reader.read_line(&mut line).unwrap()!=0{
+        print!("{line}");
+        line.clear();
+    }
+    Ok(())
 }
