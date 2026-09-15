052bc0a0d9dc09e65b1483f6c83818f733dd60ea
akarsh <akarsh98@gmail.com>
2026-09-16 00:01:49 +0200

    format...

--- c/main.c
+++ c/main.c
@@ -18,24 +18,26 @@ int printCtr = 0;
 static uint64_t hash_str(char *s)
 {
   unsigned long hash = FNV_OFFSET;
-  while (*s){
+  while (*s)
+  {
     hash ^= (uint64_t)(unsigned char)*s++;
     hash *= FNV_PRIME;
   }
   return hash;
 }
 
-typedef struct Entry{
+typedef struct Entry
+{
   char *city;
   int temperature;
-}Entry;
+} Entry;
 
 typedef struct Stats
 {
-  long sum;  
+  long sum;
   int min;
   int max;
-  int n;   // overflow? max_val is 2^31 ~2e9 ig, so should be good...
+  int n; // overflow? max_val is 2^31 ~2e9 ig, so should be good...
 } Stats;
 
 // Simple hash map with linked list collision resolution (not the best for our usage..., we try linear or quadratic probing next)
@@ -55,11 +57,12 @@ size_t citiesCount = 0;
 Stats *getOrCreateCity(char *city)
 {
   size_t idx = hash_str(city) % CAPACITY;
-  cityEntry *e =  citiesMap[idx];
+  cityEntry *e = citiesMap[idx];
 
   // follow linked list until we hit same city (can we avoid this somehow? probing?)
-  while (e != NULL){
-    if (strcmp(e->city, city)==0)
+  while (e != NULL)
+  {
+    if (strcmp(e->city, city) == 0)
       return &e->stats;
     e = e->next;
   }
@@ -73,27 +76,29 @@ Stats *getOrCreateCity(char *city)
   citiesList[citiesCount++] = e->city;
 
   // dirty magic placeholders for now..
-  e->stats = (Stats){0, 100*MULT_FACTOR, -100*MULT_FACTOR, 0};
+  e->stats = (Stats){0, 100 * MULT_FACTOR, -100 * MULT_FACTOR, 0};
   e->next = citiesMap[idx];
   citiesMap[idx] = e;
   // printf("created entry for city %s\n", city);
   return &e->stats;
-  
 }
-
 
 //--------------------- Helpers -----------------------
 
 // adapted atoi, we just ignore the '.', effectively 10x and converting to int.
 int parseTemp(const char *s)
 {
-  int n=0;
-  int neg = *s=='-'? 1:0;
+  int n = 0;
+  int neg = *s == '-' ? 1 : 0;
   s += neg;
 
-  while( (*s>='0' && *s<='9') || *s=='.'){
-    if(*s=='.'){s++;}
-    n = n*10 - (int)(*s++ - '0');
+  while ((*s >= '0' && *s <= '9') || *s == '.')
+  {
+    if (*s == '.')
+    {
+      s++;
+    }
+    n = n * 10 - (int)(*s++ - '0');
   }
   return neg ? n : -n;
 }
@@ -105,7 +110,8 @@ void parseLine(const char *line, Entry *entry)
   int start_idx = 0;
 
   // first parse city
-  while (1){
+  while (1)
+  {
     if (line[i] == ';')
     {
       len = i - start_idx;
@@ -119,49 +125,50 @@ void parseLine(const char *line, Entry *entry)
       start_idx = i + 1;
       break;
     }
-    i++;  
+    i++;
   }
 
   // parse temperature
   // we can boldy assume rest of the str is temp, parser stops at \0
-  entry->temperature = parseTemp(line+start_idx);
+  entry->temperature = parseTemp(line + start_idx);
   return;
 }
 
-
-
-void printResults(FILE* fp){
+void printResults(FILE *fp)
+{
   mergeSort(citiesList, citiesCount);
 
   fprintf(fp, "{");
-  for(size_t i=0;i<citiesCount;i++){
-    if (citiesList[i]!=NULL){
+  for (size_t i = 0; i < citiesCount; i++)
+  {
+    if (citiesList[i] != NULL)
+    {
       Stats *stats = getOrCreateCity(citiesList[i]);
-      
+
       // go through linked list to make sure we exhaust collided entries
-      fprintf(fp, "%s:%.1f/%.1f/%.1f, ", 
-        citiesList[i], 
-        (float)stats->min/MULT_FACTOR, 
-        (float)stats->sum/stats->n/MULT_FACTOR, 
-        (float)stats->max/MULT_FACTOR
-      );
+      fprintf(fp, "%s:%.1f/%.1f/%.1f, ",
+              citiesList[i],
+              (float)stats->min / MULT_FACTOR,
+              (float)stats->sum / stats->n / MULT_FACTOR,
+              (float)stats->max / MULT_FACTOR);
       printCtr++;
-      }
+    }
   }
   fprintf(fp, "}");
-}  
+}
 
 int main()
 {
   FILE *fp;
   char line[128]; // longest name's 100 chars, temp longest: 4 chars
   fp = fopen("data/measurements_1B.txt", "r");
-  
-  if (fp == NULL){
+
+  if (fp == NULL)
+  {
     printf("Error opening file!\n");
     return 1;
   }
-  
+
   while (fgets(line, sizeof(line), fp) != NULL)
   {
     Entry entry;
@@ -173,18 +180,17 @@ int main()
     stats->min = stats->min > temp ? temp : stats->min;
     stats->n += 1;
     stats->max = stats->max < temp ? temp : stats->max;
-    stats->sum += (long) temp;
+    stats->sum += (long)temp;
   }
   fclose(fp);
-  
-  fp = fopen("output/c_sol.txt","w");
-  if (fp == NULL) {
-       printf("Error opening file!");
-       return 1;
-   }
+
+  fp = fopen("output/c_sol.txt", "w");
+  if (fp == NULL)
+  {
+    printf("Error opening file!");
+    return 1;
+  }
   printResults(fp);
   fclose(fp);
   printf("%d %d", entryCtr, printCtr);
-  
 }
-    
