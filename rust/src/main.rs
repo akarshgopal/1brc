413d1a0826461593b8cd94f2f7b1e443bfacc0a6
Akarsh Gopal <akarsh.gopal@rfa.space>
2026-09-16 11:23:39 +0200

    move out to crate and lib files, bundle stats with update. switch to byte-level parsing for temperature and use i32. not a noticeable speedup though...

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,53 +1,19 @@
 use std::collections::hash_map::{HashMap};
-use std::io::{BufRead, BufReader, Write};
+use std::io::{BufRead, BufReader};
 use std::fs::File;
 
-const MULT_FACTOR: f64 = 1.0;
+mod helpers;
+use helpers::{print_results_to, parse_temperature};
+
+mod stats;
+use stats::Stats;
+
 const CAPACITY: usize = 10000;
-#[derive(Debug, Clone)]
-struct Stats{
-    min:f64,
-    max:f64,
-    sum:f64,
-    n:u64
-}
 
-impl Stats{
-    fn new()-> Stats{
-        Stats { min: f64::MAX, max: f64::MIN, sum: 0.0, n: 0 }
-    }
-}
 
-// fn print_results(cities_map: HashMap<String, Stats>){
-//     // sort the keys
-//     let mut sorted_keys: Vec<&String> = cities_map.keys().collect();
-//     sorted_keys.sort();
-
-//     for city in sorted_keys{
-//         let stats = &cities_map[city];
-//         println!("{city}:{0:.1}/{1:.1}/{2:.1}",
-//         (stats.min as f64)/(MULT_FACTOR as f64),
-//         (stats.sum as f64)/(stats.n as f64)/(MULT_FACTOR as f64), 
-//         (stats.max as f64)/(MULT_FACTOR as f64));
-//     }
-// }
-
-fn print_results_to(cities_map: HashMap<String, Stats>, f: &mut File){
-    // sort the keys
-    let mut sorted_keys: Vec<&String> = cities_map.keys().collect();
-    sorted_keys.sort();
-
-    for city in sorted_keys{
-        let stats = &cities_map[city];
-        writeln!(f,"{city}:{0:.1}/{1:.1}/{2:.1}",
-        (stats.min as f64)/(MULT_FACTOR as f64),
-        (stats.sum as f64)/(stats.n as f64)/(MULT_FACTOR as f64), 
-        (stats.max as f64)/(MULT_FACTOR as f64)).unwrap();
-    }
-}
 fn main(){
     let mut cities_map: HashMap<String, Stats> = HashMap::with_capacity(CAPACITY);
-    let path: &str = "../data/measurements_1B.txt";
+    let path: &str = "../data/measurements_10M.txt";
     let f: File = File::open(path).unwrap();
     let reader = BufReader::new(f);
     let mut line = String::new();
@@ -56,18 +22,13 @@ fn main(){
 
     for line in reader.lines(){
         let line = line.unwrap();
-        let (city, temperature) = line.split_once(';').unwrap();
-        let temperature:f64 = temperature.parse().unwrap()  ;
+        let (city, tstr) = line.split_once(';').unwrap();
+        let temperature:i32 = parse_temperature(tstr);
         // update cities_map stats
         let stats = cities_map.entry(city.to_string()).or_insert(
             Stats::new()
         );
-
-        stats.max = stats.max.max(temperature);
-        stats.min = stats.min.min(temperature);
-        stats.sum += temperature; 
-        stats.n+= 1;
-
+        stats.update(temperature);
     }
 
     let mut f = File::create("../output/rs_sol.txt").unwrap();
